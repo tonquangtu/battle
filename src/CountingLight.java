0d7ce3a5f123240d4df2f1b91d729efe5df435cd
tuton <tu.ton@tiki.vn>
2019-05-23 22:28:36 +0700

    Counting light Axon

---
+++ src/CountingLight.java
@@ -0,0 +1,42 @@
+public class CountingLight {
+
+    public static void main(String[] args) {
+        var counter = new CountingLight();
+//        var input = new int [] {2, 1, 3, 5, 4};
+        var input = new int [] {5, 4, 3, 2, 1, 6};
+        var result = counter.counting(input);
+        System.out.println(result);
+    }
+
+    public int counting(int[] light) {
+        boolean[] mark = new boolean[light.length + 1]; // to mark light i is on or off
+        boolean[] visit = new boolean[light.length + 1]; // to mark light i is visit or not
+        for (int i = 0; i <= light.length; i++) {
+            mark[i] = false; // turn of the light
+            visit[i] = false; // mark i not visit
+        }
+
+        int count = 0;
+        for (int i = 0; i < light.length; i++) {
+            visit[light[i]] = true;
+            if (light[i] == 1 || mark[light[i] - 1]) {
+                count += 1;
+                turnOnLights(light, mark, visit, i);
+            }
+        }
+        return count;
+    }
+
+    public void turnOnLights(int[] light, boolean[] mark, boolean[] visit, int index) {
+        mark[light[index]] = true; // turn on this light and turn on lights after this light
+        int prev = light[index] + 1;
+        while (prev <= light.length) {
+            if (!mark[prev] && visit[prev]) {
+                mark[prev] = true;
+                prev += 1;
+            } else {
+                break;
+            }
+        }
+    }
+}
